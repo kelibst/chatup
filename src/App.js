e20d6630d3eb3d86b8ebad67219c235d20d42f7f
kelibst <kboster17@gmail.com>
2020-10-01 10:44:26 +0000

    setup folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h4 className="text-center">Hello World</h4>
     </div>
+    </BrowserRouter>
   );
 }
 
